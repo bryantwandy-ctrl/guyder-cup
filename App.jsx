69c046aa71e10f0e2ab6e5f1b7a90eb0306da70e
bryantwandy-ctrl <bryantwandy@gmail.com>
2026-07-03 11:37:08 -0500

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -65,6 +65,12 @@ function BrandFontLoader() {
           grid-template-columns: 1fr !important;
         }
       }
+      input[type=number]::-webkit-inner-spin-button,
+      input[type=number]::-webkit-outer-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+      }
+      input[type=number] { -moz-appearance: textfield; }
     `}</style>
   );
 }
